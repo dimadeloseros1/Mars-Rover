f3cdab70ea598268109e9d70e3c36292d2cc4cff
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-08-16 15:42:46 +0100

    Feat: Implement multiple rotation rover method

--- src/main/java/org/example/Actions/GameManager.java
+++ src/main/java/org/example/Actions/GameManager.java
@@ -3,17 +3,52 @@ package org.example.Actions;
 import org.example.Enums.CompassDirection;
 import org.example.Enums.InstructionsVals;
 
+import java.util.List;
+
 public class GameManager {
 
-    public CompassDirection rotate(CompassDirection direction, InstructionsVals instructions) {
-        if (direction == CompassDirection.N && instructions == InstructionsVals.R) {
-            return CompassDirection.E;
-        } else if (direction == CompassDirection.S && instructions == InstructionsVals.L) {
-            return CompassDirection.W;
-        } else if (direction == CompassDirection.E && instructions == InstructionsVals.R) {
-            return CompassDirection.S;
-        } else {
-            return CompassDirection.N;
+    public CompassDirection rotate(CompassDirection direction, List<InstructionsVals> instructions) {
+        var compassDirection = direction;
+
+        for (InstructionsVals instr : instructions) {
+            switch (compassDirection) {
+                case N:
+                    if (instr == InstructionsVals.R) {
+                        direction = CompassDirection.E;
+                        System.out.println("You are currently on EAST");
+                    } else if (instr == InstructionsVals.L) {
+                        direction = CompassDirection.W;
+                        System.out.println("You are currently on WEST");
+                    }
+                    break;
+
+                case S:
+                    if (instr == InstructionsVals.R) {
+                        direction = CompassDirection.W;
+                        System.out.println("You are currently on WEST");
+                    } else if (instr == InstructionsVals.L) {
+                        direction = CompassDirection.E;
+                        System.out.println("You are currently on EAST");
+                    }
+                    break;
+
+                case E:
+                    if (instr == InstructionsVals.R) {
+                        direction = CompassDirection.S;
+                    } else if (instr == InstructionsVals.L) {
+                        direction = CompassDirection.N;
+                    }
+                    break;
+
+                case W:
+                    if (instr == InstructionsVals.R) {
+                        direction = CompassDirection.N;
+                    } else if (instr == InstructionsVals.L) {
+                        direction = CompassDirection.S;
+                    }
+                    break;
+            }
         }
+        return direction;
     }
 }
