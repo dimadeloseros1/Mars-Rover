82b9aff019909adfa8074fd499787f4421a1410a
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-08-16 10:17:41 +0100

    Feat: Implement methods for directions & instructions

--- src/main/java/org/example/Actions/ConsoleIO.java
+++ src/main/java/org/example/Actions/ConsoleIO.java
@@ -1,4 +1,27 @@
 package org.example.Actions;
 
+import org.example.Enums.CompassDirection;
+import org.example.Enums.InstructionsVals;
+
 public class ConsoleIO {
+
+    public static CompassDirection compassDirection(String input) {
+        return switch (input) {
+            case "N" -> CompassDirection.N;
+            case "S" -> CompassDirection.S;
+            case "E" -> CompassDirection.E;
+            case "W" -> CompassDirection.W;
+            default -> throw new IllegalStateException("Unexpected value: " + input);
+        };
+    }
+
+    public static InstructionsVals instructionsVals(String input) {
+        return switch (input) {
+            case "L" -> InstructionsVals.L;
+            case "R" -> InstructionsVals.R;
+            case "M" -> InstructionsVals.M;
+            default -> throw new IllegalStateException("Unexpected value: " + input);
+        };
+    }
+
 }
