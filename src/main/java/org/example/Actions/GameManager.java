74ac95bfed91be312a4c3e1fffe0d7bcecd3ed10
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-08-16 10:15:30 +0100

    Feat: Implement rotate method

--- src/main/java/org/example/Actions/GameManager.java
+++ src/main/java/org/example/Actions/GameManager.java
@@ -1,4 +1,19 @@
 package org.example.Actions;
 
+import org.example.Enums.CompassDirection;
+import org.example.Enums.InstructionsVals;
+
 public class GameManager {
+
+    public CompassDirection rotate(CompassDirection direction, InstructionsVals instructions) {
+        if (direction == CompassDirection.N && instructions == InstructionsVals.R) {
+            return CompassDirection.E;
+        } else if (direction == CompassDirection.S && instructions == InstructionsVals.L) {
+            return CompassDirection.W;
+        } else if (direction == CompassDirection.E && instructions == InstructionsVals.R) {
+            return CompassDirection.S;
+        } else {
+            return CompassDirection.N;
+        }
+    }
 }
