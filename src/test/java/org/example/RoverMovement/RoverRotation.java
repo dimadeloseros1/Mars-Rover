4684f33c56fac73f7d93429b775e98b07e5729e9
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-08-16 15:44:19 +0100

    Test: Redisign valid & invalid rover movements tests

--- src/test/java/org/example/RoverMovement/RoverRotation.java
+++ src/test/java/org/example/RoverMovement/RoverRotation.java
@@ -1,7 +1,9 @@
 package org.example.RoverMovement;
 
+import org.example.Actions.GameManager;
 import org.example.Enums.CompassDirection;
 import org.example.Enums.InstructionsVals;
+import org.example.Parsers.InputParser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,18 +16,27 @@ public class RoverRotation {
     @Test
     @DisplayName("This method will check whether the rover rotation works accordingly")
     public void RoverMovementValidDirection() {
-        var rover = new Rover();
-        var roverMovement = rover.rotate(CompassDirection.N, InstructionsVals.R);
+        var rover = new GameManager();
+        var movingLeft = InputParser.instructionParser("l");
+        var movingRight = InputParser.instructionParser("r");
 
-        assertEquals(CompassDirection.E, roverMovement);
+        var onNorthAndMovingLeft = rover.rotate(CompassDirection.N, movingLeft);
+        var onEastAndMovingRight = rover.rotate(CompassDirection.E, movingRight);
+
+        assertEquals(CompassDirection.W, onNorthAndMovingLeft);
+        assertEquals(CompassDirection.S, onEastAndMovingRight);
     }
 
     @Test
     @DisplayName("This method will check for invalid compass direction")
     public void RoverMovementInvalidDirection() {
-        var rover = new Rover();
-        var roverMovement = rover.rotate(CompassDirection.N, InstructionsVals.R);
+        var rover = new GameManager();
+        var instructionParser = InputParser.instructionParser("l");
 
+        var roverMovement = rover.rotate(CompassDirection.N, instructionParser);
+
+        assertNotEquals(CompassDirection.E, roverMovement);
+        assertNotEquals(CompassDirection.N, roverMovement);
         assertNotEquals(CompassDirection.S, roverMovement);
     }
 
