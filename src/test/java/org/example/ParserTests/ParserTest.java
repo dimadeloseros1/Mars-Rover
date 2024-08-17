84a02e2296186a2911cd24b3b243fee3d416e90e
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-08-17 20:35:33 +0100

    Test: Implement assertThrows test for invalid plateau size

--- src/test/java/org/example/ParserTests/ParserTest.java
+++ src/test/java/org/example/ParserTests/ParserTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.ParserTests;
 
 import org.example.Actions.PlateauSize;
 import org.example.Actions.Position;
@@ -8,9 +8,9 @@ import org.example.Parsers.InputParser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,7 +27,7 @@ public class ParserTest {
     @Test
     @DisplayName("Testing for correct plateau size")
     public void testValidPlateauSize() {
-        var expectedOutput = InputParser.plateauSizeParser("55");
+        var expectedOutput = InputParser.plateauSizeParser("5 5");
         var expectedInput = new PlateauSize(5,5);
 
         assertEquals(expectedOutput.getX(), expectedInput.getX());
@@ -37,18 +37,16 @@ public class ParserTest {
     @Test
     @DisplayName("Testing for incorrect plateau size")
     public void testInvalidPlateauSize() {
-        var expectedOutput = InputParser.plateauSizeParser("55");
-        var expectedInput = new PlateauSize(5,5);
+        Executable executable = () -> InputParser.plateauSizeParser("4 5");
 
-        assertEquals(expectedOutput.getX(), expectedInput.getX());
-        assertEquals(expectedOutput.getY(), expectedInput.getY());
+        assertThrows(IllegalArgumentException.class, executable, "Expected IllegalArgumentException for out-of-range input.");
     }
 
     @Test
     @DisplayName("Testing for correct position")
     public void testValidCompassDirection() {
 
-        var input = InputParser.positionParser("23s");
+        var input = InputParser.positionParser("2 3 S");
         var expectedOutput = new Position(2,3, CompassDirection.S);
 
         assertEquals(expectedOutput.getX(), input.getX());
@@ -60,7 +58,7 @@ public class ParserTest {
     @DisplayName("Testing for invalid position")
     public void testsInvalidCompassDirection() {
 
-        var input = InputParser.positionParser("23w");
+        var input = InputParser.positionParser("2 3 w");
         var expectedOutput = new Position(4,2, CompassDirection.S);
 
         assertNotEquals(expectedOutput.getX(), input.getX());
